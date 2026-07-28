e3e6e4cdc68d142c4152a2291bdf4be1ecdd1e2d
Thanu624 <p04262055@gmail.com>
2026-07-28 18:21:47 +0530

    Time: 110 ms (49.09%), Space: 61.5 MB (26.9%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        ans = []
+        path = []
+
+        def backtrack(start: int) -> None:
+
+            if len(path) == k:
+                ans.append(path.copy())
+                return
+
+            for num in range(start, n + 1):
+                path.append(num)
+                backtrack(num + 1)
+                path.pop()
+
+        backtrack(1)
+
+        return ans
